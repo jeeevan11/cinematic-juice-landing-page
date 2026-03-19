2ccdb4e4bb87037b8eba8d1523b0a544589e0254
Jatin Chhanwal <connectwithjatin365@gmail.com>
2026-03-20 00:45:39 +0530

    Push

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/cinematic-juice-landing-page/',
+  base: process.env.GITHUB_ACTIONS ? '/cinematic-juice-landing-page/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
